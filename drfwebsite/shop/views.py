9578f9c28331122c391a4a145eef7801321c4b84
Dannys 132 <Danlucky2003@gmail.com>
2022-08-13 18:31:42 +0300

    Added pagination

--- drfwebsite/shop/views.py
+++ drfwebsite/shop/views.py
@@ -3,10 +3,16 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView, status
+from rest_framework.pagination import PageNumberPagination
 
 from .serializers import ProductSerialaizer
 from .models import Product
 
+
+class ProductListPagination(PageNumberPagination):
+    page_size = 3
+    page_query_param = 'page_size'
+    max_page_size = 10000
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
@@ -15,6 +21,7 @@ class ProductViewSet(viewsets.ModelViewSet):
 class ProductAPIList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerialaizer
+    pagination_class = ProductListPagination
 
 class ProductAPIUpdate(generics.UpdateAPIView):
     queryset = Product.objects.all()
